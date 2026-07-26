dab7e05be1dc4cfababfaa6675c7a560e06dbacc
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-07-26 09:23:40 +0400

    First release

---
+++ Baseera/src/test/java/com/example/Baseera/BaseeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Baseera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0caf0885617254298654e01c1b9456c6fc351c06
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-07-26 23:01:54 +0400

    added Plan class

---
+++ Baseera/src/main/java/entity/Plan.java
@@ -0,0 +1,33 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@SuperBuilder
+public class Plan extends BaseEntity {
+
+    @Column(nullable = false, length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String condition; // "ASD" or "ADHD"
+
+    @Column(nullable = false)
+    private Integer minAgeMonths;
+
+    @Column(nullable = false)
+    private Integer maxAgeMonths;
+
+    @OneToMany(mappedBy = "plan", fetch = FetchType.LAZY)
+    private List<Child> children = new ArrayList<>();
+}

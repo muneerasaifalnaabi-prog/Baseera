5c4b7b7c3bb936faa8e6c11cacb067772a8094b4
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-26 16:45:42 +0400

    add child entity

--- Baseera/src/main/java/entities/Child.java
+++ Baseera/src/main/java/entities/Child.java
@@ -1,4 +1,22 @@
 package entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Child {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private boolean isActive;
 }

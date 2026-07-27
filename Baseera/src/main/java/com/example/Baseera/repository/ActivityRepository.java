77d3d8cc9b57cf99f2e2b723fdc24267437d098d
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-07-27 11:43:41 +0400

    Added Activiy Repository

---
+++ Baseera/src/main/java/com/example/Baseera/repository/ActivityRepository.java
@@ -0,0 +1,7 @@
+package com.example.Baseera.repository;
+
+import com.example.Baseera.entity.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+}

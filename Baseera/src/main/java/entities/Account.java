94a609acae1c331bf6ba479645f6fcc794a57453
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-27 07:53:34 +0400

    add Account class

--- Baseera/src/main/java/entities/Account.java
+++ Baseera/src/main/java/entities/Account.java
@@ -1,4 +1,13 @@
 package entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
 }

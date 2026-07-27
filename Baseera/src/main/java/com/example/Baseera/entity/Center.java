e393df76435f4073b33f04e295709b59a5b916c6
Shahd AL Abdali <shahad9mubarak@gmail.com>
2026-07-27 08:40:12 +0400

    add Center entity

--- Baseera/src/main/java/com/example/Baseera/entity/Center.java
+++ Baseera/src/main/java/com/example/Baseera/entity/Center.java
@@ -1,4 +1,26 @@
 package com.example.Baseera.entity;
 
-public class Center {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Center extends BaseEntity{
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String specialty;
+
+    @Column(nullable = false)
+    private String phone;
+
 }

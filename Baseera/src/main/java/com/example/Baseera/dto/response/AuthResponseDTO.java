abf33acac6439d8052ef28c0a9ec74576b927dff
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-07-28 11:03:49 +0400

    added register/login request dtos with their field &AuthResponse

--- Baseera/src/main/java/com/example/Baseera/dto/response/AuthResponseDTO.java
+++ Baseera/src/main/java/com/example/Baseera/dto/response/AuthResponseDTO.java
@@ -1,4 +1,27 @@
 package com.example.Baseera.dto.response;
 
+import com.example.Baseera.entity.Account;
+import com.example.Baseera.enums.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class AuthResponseDTO {
+
+    private String token;
+    private String fullName;
+    private String email;
+    private Role role;
+
+    public static AuthResponseDTO fromEntity(Account account, String token) {
+        AuthResponseDTO dto = new AuthResponseDTO();
+
+        dto.setToken(token);
+        dto.setFullName(account.getFullName());
+        dto.setEmail(account.getEmail());
+        dto.setRole(account.getRole());
+
+        return dto;
+    }
 }

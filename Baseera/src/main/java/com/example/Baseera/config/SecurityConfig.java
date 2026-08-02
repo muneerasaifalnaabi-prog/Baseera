b282addc29487c16acf72bb78ba4901296954cd5
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-08-02 21:40:25 +0400

    Fix the bugs

--- Baseera/src/main/java/com/example/Baseera/config/SecurityConfig.java
+++ Baseera/src/main/java/com/example/Baseera/config/SecurityConfig.java
@@ -76,16 +76,4 @@ public class SecurityConfig {
             AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
